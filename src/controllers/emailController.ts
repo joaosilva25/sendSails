88dd289be41def7014b3bf6cc24f0e1553e331c5
JoaoSilva25 <joaosilva2505@outlook.com>
2023-11-22 10:39:00 -0300

    switch text to html

--- src/controllers/emailController.ts
+++ src/controllers/emailController.ts
@@ -7,7 +7,7 @@ dotenv.config()
 export const send=async(req:Request, res:Response)=> {
 
     
-    let {userAuth,passAuth,from,to,subject,text}=req.body
+    let {userAuth,passAuth,from,to,subject,html}=req.body
 
     var transport = nodemailer.createTransport({
         host: "smtp.gmail.com",
@@ -20,12 +20,12 @@ export const send=async(req:Request, res:Response)=> {
     });
 
 
-    if(from && to && subject && text) {
+    if(from && to && subject && html) {
         let message = {
             from,
             to,
             subject,
-            text
+            html
         }
 
         try {
