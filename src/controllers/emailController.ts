840f5a5cd4a2eb53c00a771e1c17d163e928ce2d
JoaoSilva25 <joaosilva2505@outlook.com>
2023-11-22 16:57:26 -0300

    fix

--- src/controllers/emailController.ts
+++ src/controllers/emailController.ts
@@ -6,7 +6,7 @@ dotenv.config()
 
 export const send=async(req:Request, res:Response)=> {
 
-    
+
     let {userAuth,passAuth,from,to,subject,html}=req.body
 
     var transport = nodemailer.createTransport({
@@ -18,6 +18,7 @@ export const send=async(req:Request, res:Response)=> {
             pass: passAuth
         }
     });
+
 
         let message = {
             from,
